6ca3b27dc13bfdf9b4cd8d5a3c940790082a70ee
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2024-09-29 11:37:43 +0800

    hello

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -2,11 +2,11 @@
 
 __global__ void hello(){
 
-  printf("Hello from block: %u, thread: %u\n", FIXME);
+  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
 int main(){
 
-  hello<<<FIXME>>>();
+  hello<<<2,2>>>();
   cudaDeviceSynchronize();
 }
