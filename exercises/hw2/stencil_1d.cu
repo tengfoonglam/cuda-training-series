dd2701d069df6136dc1a0fdccad1bfdc9b190ee3
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2024-10-06 21:57:57 +0800

    refactor

--- exercises/hw2/stencil_1d.cu
+++ exercises/hw2/stencil_1d.cu
@@ -6,6 +6,7 @@ using namespace std;
 #define N 4096
 #define RADIUS 3
 #define BLOCK_SIZE 16
+#define PADDED_ARRAY_SIZE 2 * RADIUS + N
 
 __global__ void stencil_1d(int *in, int *out) {
   __shared__ int temp[BLOCK_SIZE + 2 * RADIUS];
@@ -27,7 +28,7 @@ __global__ void stencil_1d(int *in, int *out) {
 
   // Apply the stencil
   int result = 0;
-  for (int offset = -RADIUS; offset <= RADIUS; offset++) {
+  for (int offset = -RADIUS; offset <= RADIUS; ++offset) {
     result += temp[lindex + offset];
   }
 
@@ -42,36 +43,39 @@ int main(void) {
   int *d_in, *d_out; // device copies of a, b, c
 
   // Alloc space for host copies and setup values
-  int size = (N + 2 * RADIUS) * sizeof(int);
-  in = (int *)malloc(size);
-  fill_ints(in, N + 2 * RADIUS);
-  out = (int *)malloc(size);
-  fill_ints(out, N + 2 * RADIUS);
+  static constexpr int PADDED_ARRAY_SIZE_BYTES =
+      (PADDED_ARRAY_SIZE) * sizeof(int);
+  in = (int *)malloc(PADDED_ARRAY_SIZE_BYTES);
+  fill_ints(in, PADDED_ARRAY_SIZE);
+  out = (int *)malloc(PADDED_ARRAY_SIZE_BYTES);
+  fill_ints(out, PADDED_ARRAY_SIZE);
 
   // Alloc space for device copies
-  cudaMalloc((void **)&d_in, size);
-  cudaMalloc((void **)&d_out, size);
+  cudaMalloc((void **)&d_in, PADDED_ARRAY_SIZE_BYTES);
+  cudaMalloc((void **)&d_out, PADDED_ARRAY_SIZE_BYTES);
 
   // Copy to device
-  cudaMemcpy(d_in, in, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_out, out, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_in, in, PADDED_ARRAY_SIZE_BYTES, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_out, out, PADDED_ARRAY_SIZE_BYTES, cudaMemcpyHostToDevice);
 
   // Launch stencil_1d() kernel on GPU
   // Offset by RADIUS so it starts on the first value and not the padding
   stencil_1d<<<N / BLOCK_SIZE, BLOCK_SIZE>>>(d_in + RADIUS, d_out + RADIUS);
 
   // Copy result back to host
-  cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(out, d_out, PADDED_ARRAY_SIZE_BYTES, cudaMemcpyDeviceToHost);
 
   // Error Checking
-  for (int i = 0; i < N + 2 * RADIUS; i++) {
+  for (int i = 0; i < PADDED_ARRAY_SIZE; ++i) {
     if (i < RADIUS || i >= N + RADIUS) {
-      if (out[i] != 1)
+      if (out[i] != 1) {
         printf("Mismatch at index %d, was: %d, should be: %d\n", i, out[i], 1);
+      }
     } else {
-      if (out[i] != 1 + 2 * RADIUS)
+      if (out[i] != 1 + 2 * RADIUS) {
         printf("Mismatch at index %d, was: %d, should be: %d\n", i, out[i],
                1 + 2 * RADIUS);
+      }
     }
   }
 
